6476f922aa348e9fef598c6b7f2021cfd26f738b
AleksanderS <aleksander.sie@gmail.com>
2023-09-25 00:18:07 +0300

    new text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>AUTODEPLOY Vite + React</h1>
+      <h1>AUTODEPLOY IS WORK Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
